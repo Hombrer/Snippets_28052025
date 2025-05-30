50c04dc90548ca66a253d3b1826f74df7b6c3031
Hombrer <5208643@mail.ru>
2025-05-30 10:37:57 +0300

    Change login form and add error login handling

--- MainApp/views.py
+++ MainApp/views.py
@@ -134,7 +134,11 @@ def login(request):
             auth.login(request, user)
         else:
             # Return error message
-            pass
+            context = {
+                "pagename": "PythonBin",
+                "errors": ["Wrong username or password"]
+            }
+            return render(request, "pages/index.html", context)
     return redirect('home')
 
 
